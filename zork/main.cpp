8f3d5dbf7ea89c2ed46b1cd8692e85ded5afa16f
Rodrigo Samayoa <toti1998sama@gmail.com>
2022-02-27 17:43:15 -0600

    Changes to initialText function and main

--- zork/main.cpp
+++ zork/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <string>
-#include <vector>
 #include "world.h"
 
 using namespace std;
@@ -15,14 +14,11 @@ void initialText()
 	cout << "Welcome to Lab Zork!"
 		 << "\n";
 	cout << "\n";
-	cout << "\n";
-	cout << "You are just waking up!"
-		 << "\n";
-	cout << "You have been kidnapped."
+	cout << "You are just waking up! You have been kidnapped."
 		 << "\n";
 	cout << "You are looking around and it seems you are inside a laboratory."
 		 << "\n";
-	cout << "What they were doing to you and wheres the people? Why is there no one here?."
+	cout << "What they were doing to you? Why is there no one here?"
 		 << "\n";
 	cout << "\n";
 }
@@ -32,13 +28,19 @@ int main()
 	initialText();
 
 	World world;
-
 	string input;
 
 	bool exit = true;
 	while (exit)
 	{
 		std::getline(std::cin, input);
+
+		if(!world.ValidateInput(input))
+		{
+			cout << "Thats something I dont understand."
+ 				 << "\n"
+ 				 << "\n";
+		}
 	}
 
 	return 0;
