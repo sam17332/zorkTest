73c879fef7fb1dee402e29856bac7025c17a04d5
Rodrigo Samayoa <toti1998sama@gmail.com>
2022-02-27 19:11:26 -0600

    Add new properties and function declarations

--- zork/player.h
+++ zork/player.h
@@ -1,9 +1,13 @@
 #ifndef _Player
 #define _Player
 
+#include <vector>
 #include "creature.h"
 
 using namespace std;
+
+class Room;
+class Item;
 
 class Player : public Creature
 {
@@ -14,10 +18,20 @@ public:
     Player(const char *name, const char *description, Room *room);
     ~Player();
 
-    bool hasKey;
-    bool isTied;
+    string GetRoomsName();
+    void Untie();
+    void ChangeRoom(Room *room);
+    void TakeItem(Item *item, string roomName);
+    void DropItem(Item *item, Room *room);
+    void ShowInventory();
 
 public:
+    bool hasKey;
+    bool hasKeyCard;
+    bool isTied;
+    bool inPresent;
+    Room *room;
+    vector<Item *> items;
 };
 
 #endif //_Player
